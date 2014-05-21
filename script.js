9133a86bef5284363d3401ac451cf460bda88278
aaguil10 <aaguil10@ucsc.edu>
2014-05-21 15:11:44 -0700

    version I sent team... ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 
 //question object
 function Question(id,val,slot) {
-	this.id = id;
-	this.val = val;
-	this.slot = slot;
+	this.id = id;		//stores element id
+	this.val = val;		//stores value in input field
+	this.slot = slot;	//stores the order that the questions in
 }
 
 //Section object stores an array of questions
 function Section(id,slot){
-	this.id = id;
-	this.q = [];
-	this.slot = slot;
-	this.count = 0;
-	this.slot_finder = [];
+	this.id = id;			//stores id of section
+	this.q = [];			//stores array of questions q[0] being the first q in the survey
+	this.slot = slot;		//use to keep track of the order of elements
+	this.count = 0;			//count of questions made so far
+	this.slot_finder = [];	//used as a hash to find slot value of a question from id
 }
 
 //Survey object stores an array of sections
 function Survey(){
-	this.s = [];
-	this.count = 0;
-	this.slot_finder = [];
+	this.s = [];			//array of sections
+	this.count = 0;			//count of sections made so far
+	this.slot_finder = [];	//slot_finder[section_id] will give section_slot (see above)
 }
 
+//function to update questions
 Survey.prototype.editquestion = function (sec_id, q_id, new_val) {
   var sec_slot = mySurvey.slot_finder[sec_id];
   var q_slot = mySurvey.s[sec_slot].slot_finder[q_id];
@@ -29,7 +30,7 @@ Survey.prototype.editquestion = function (sec_id, q_id, new_val) {
   //alert("value changed to " + mySurvey.s[sec_slot].q[q_slot].val);
 };
 
-
+//the main form object
 var mySurvey = new Survey();
 
 //global variables for selectors
@@ -46,34 +47,34 @@ function change_selection(new_id){
 	$('#' + curr_selcetion).css('border','1px solid blue');
 	
 	//makes only the current selection sortable
-	$('#' + new_id).sortable({
+	$('.sections').sortable({
 		update: function(event, ui) {
-			//var Order = $(".sections").sortable('toArray').toString();
-			var arr = $('#' + new_id).sortable('toArray');
-			var sec_slot = mySurvey.slot_finder[curr_selcetion];
-			mySurvey.s[sec_slot].q = update_ques_order(arr, curr_selcetion);
-			alert(mySurvey.s[sec_slot].q[0].val);
-			//var clill = $('#' + arr[1]).children('input').val();
-			//alert(clill);
+			//alert($(this).attr('id'));
+			var arr = $(this).sortable('toArray');
+			var sec_slot = mySurvey.slot_finder[$(this).attr('id')];
+			//alert(sec_slot);
+			mySurvey.s[sec_slot].q = update_ques_order(arr, $(this).attr('id'));
+			//alert(mySurvey.s[sec_slot].q[0].id);
+
 		}
 	
 	});
-	$('#' + new_id).disableSelection();
+	$('.sections').disableSelection();
 }
 
+//Known bugs: can't update the value of slot in each question object
 function update_ques_order(array, sec_id){
 	var i = 2;
 	var j = 0;
 	var newOrder = []; 
-	//mySurvey.s[0].q[0].val
 	var sec_slot = mySurvey.slot_finder[sec_id];
 	
-	//alert("Section: " + sec_id);// mySurvey.s[sec_id].id);// + " Question: " + mySurvey.s[sec_id].q[0].id);
 	while(i <= array.length){
 		
 		var oldSlot = mySurvey.s[sec_slot].slot_finder[array[i]];
 		newOrder[j++] = mySurvey.s[sec_slot].q[oldSlot];
-
+		//newOrder[j-1].val = j-1;
+		//alert("Q: " + newOrder[0].val);
 	i++;
 	}
 	return newOrder;
@@ -83,7 +84,6 @@ function update_ques_order(array, sec_id){
 //creates new empty section
 function make_section(id_num){
 	var section = document.createElement("ul");
-	//section.innerHTML = "<h4>Section Name<h4>";
 	section.setAttribute('class', 'sections');
 	section.setAttribute('id', 'section_' + id_num);
 	section.setAttribute('onclick', 'change_selection(this.id)');
@@ -120,7 +120,7 @@ function make_question(id_num, sec_id, sec_slot){
 }
 
 
-//code to and sections and questions
+//code to add sections and questions
 function add_section(){
 	tmp_selcetion = 'section_' + sec_count;
 	$('#form_display').append(make_section(sec_count++));
@@ -138,16 +138,13 @@ function add_question(){
 }
 
 
+//makes sections sortable
 $(function() {
 	$('#form_display').sortable();
-	$('#form_display').disableSelection();
-	
+	$('#form_display').disableSelection();	
 
 });
 
-$( ".section" ).focus(function() {
-  alert( "A section is in focus" );
-});
 
 
 //miscellaneous function for debugging
